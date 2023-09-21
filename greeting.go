a5de9e493b8d6929306e5131957d372f6f0bbade
eitanby <eitanb@orpak.com>
2023-09-21 17:21:41 +0300

    change again

--- greeting.go
+++ greeting.go
@@ -1,8 +1,14 @@
 package greeting
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!?!?!?!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf("Hi, %v. You are welcome!", name)
+	return message, nil
 }
